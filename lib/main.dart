23ead36c1779631a470b895713067e82ca7ec02b
chutiwat.ro <chutiwat.ro@throughwave.co.th>
2021-12-06 21:55:59 +0700

    demo: adding demo of Provider global state management in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(Quizzler());
+void main() => runApp(ProviderDemo());
 
-class Quizzler extends StatelessWidget {
+class ProviderDemo extends StatelessWidget {
+  const ProviderDemo({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
-        ),
+    return ChangeNotifierProvider<TextTransformContext>(
+      create: (_) => TextTransformContext(),
+      child: MaterialApp(
+        home: WidgetLevel1(),
       ),
     );
   }
 }
 
-class QuizPage extends StatefulWidget {
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
+class WidgetLevel1 extends StatelessWidget {
+  const WidgetLevel1({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('His Mom'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('this is level 1 of widget tree'),
+          WidgetLevel2(),
+          SizedBox(
+            width: double.infinity,
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        //TODO: Add a Row here as your score keeper
-      ],
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: (() {
+          // When using Provider in onPressed Function you need to set listen: false
+          // to prevent Provider in onPressed reupdate.
+          Provider.of<TextTransformContext>(context, listen: false)
+              .toggleTransform();
+        }),
+      ),
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+class WidgetLevel2 extends StatelessWidget {
+  const WidgetLevel2({Key? key}) : super(key: key);
+  final String text = 'This is Level 2 of widget tree with MiXeD CaPiTaLiZe.';
+  @override
+  Widget build(BuildContext context) {
+    String transform = Provider.of<TextTransformContext>(context).textTransform;
+    bool isUpperCase = transform == 'uppercase';
+    bool isLowerCase = transform == 'lowercase';
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      width: 100.0,
+      height: 100.0,
+      color: Colors.cyan,
+      child: Text(isUpperCase
+          ? text.toUpperCase()
+          : isLowerCase
+              ? text.toLowerCase()
+              : text),
+    );
+  }
+}
+
+// Change Notifier is from Flutter by default when value of this class has been
+// Changed and run notifyListeners() it'll notify all widget who use this data
+// it's gonna merging with Provider that like global state management in redux.
+class TextTransformContext extends ChangeNotifier {
+  String textTransform = '';
+  void toggleTransform() {
+    textTransform = textTransform == 'lowercase' ? 'uppercase' : 'lowercase';
+    // Notify new value to all widget to rebuild of changed.
+    notifyListeners();
+  }
+}
